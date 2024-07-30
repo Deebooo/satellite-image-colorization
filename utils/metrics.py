4fed0eca177ba74e2e70c290bee79ae7d579d1f9
Adem <djemai.adem.merouane@gmail.com>
2024-07-30 17:28:42 +0200

    implemented torchmetrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -5,7 +5,7 @@ from torchmetrics.functional import recall as recall_metric
 from torchmetrics.functional import f1_score as f1_score_metric
 from torchmetrics.functional import jaccard_index as jaccard_metric
 from torchmetrics.functional import accuracy as accuracy_metric
-from torchmetrics.image import StructuralSimilarityIndexMeasure as ssim_metric
+from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 import torch.nn.functional as F
 
@@ -38,15 +38,22 @@ def calculate_metrics(generator, dataloader, device):
             jaccard = jaccard_metric(gen_binary, real_binary, task="binary").to(device)
             accuracy = accuracy_metric(gen_binary, real_binary, task="binary").to(device)
 
-            # Calculate SSIM using torchmetrics
-            ssim = ssim_metric(gen_color, real_color, data_range=1.0).to(device)
+            # Calculate SSIM for each image in the batch /!\
+            real_color_np = real_color.cpu().numpy()
+            gen_color_np = gen_color.cpu().numpy()
+            for i in range(gen_color_np.shape[0]):
+                ssim_score = ssim(real_color_np[i].transpose(1, 2, 0),
+                                  gen_color_np[i].transpose(1, 2, 0),
+                                  multichannel=True, data_range=gen_color_np[i].max() - gen_color_np[i].min(),
+                                  win_size=11, channel_axis=2)
+                ssim_scores.append(ssim_score)
 
             precisions.append(precision.item())
             recalls.append(recall.item())
             f1s.append(f1.item())
             jaccard_scores.append(jaccard.item())
             accuracies.append(accuracy.item())
-            ssim_scores.append(ssim.item())
+            ssim_scores.append(ssim)
 
             # Calculate PSNR for the entire batch
             mse = F.mse_loss(gen_color, real_color).item()
@@ -58,6 +65,6 @@ def calculate_metrics(generator, dataloader, device):
         'recall': torch.mean(torch.tensor(recalls)).item(),
         'f1': torch.mean(torch.tensor(f1s)).item(),
         'psnr': np.mean(psnr_values),
-        'ssim': torch.mean(torch.tensor(ssim_scores)).item(),
+        'ssim': np.mean(ssim_scores),
         'jaccard': torch.mean(torch.tensor(jaccard_scores)).item()
     }
